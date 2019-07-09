798b09666a61402aeb07f0f3d40c30e5b826bd61
Aluno <you@example.com>
2019-07-09 20:12:18 -0300

    Parte de implementação de usuários qause concluido

--- login/urls.py
+++ login/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home)
-    
+    path('', views.home),
+    path('login/', views.login_list),
+    path('login/delete/<int:login_id>/', views.login_delete),
     ]
--- login/views.py
+++ login/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
+from . models import User
 
 # Create your views here.
 
 def home(request):
     return render (request, 'home.html', {})
+
+def login_list(request):
+    users = User.objects.all()
+    return render (request, 'users/list.html', {'users': users})
+def login_delete(request):
+    user = User.objects.get( pk = user_id)
+    user.delete()
+    return redirect('/login/login/')
